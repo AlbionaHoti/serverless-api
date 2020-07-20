a394b50f4f348f793d43647b43f51b0b0e5255e9
albionahoti <albionahh@outlook.com>
2020-07-20 21:13:55 +0200

    update tdd

--- api/habits-tracker/__tests__/crud.test.js
+++ api/habits-tracker/__tests__/crud.test.js
@@ -65,7 +65,7 @@ describe("CRUD Test", () => {
                                 id: habit2.data.habits.createHabit.data.id,
                                 title: "Habit 2",
                                 description: "This is my 2nd habit.",
-                                habitScore: 2
+                                habitScore: null
                             },
                             {
                                 id: habit1.data.habits.createHabit.data.id,
@@ -87,14 +87,14 @@ describe("CRUD Test", () => {
             body: {
                 query: CREATE_HABIT,
                 variables: {
-                    data: { description: "This is my 1st habit.", habitScore: 1 }
+                    data: { description: "This is my 1st habit.", habitScore: null }
                 }
             }
         });
 
         let [error] = body.errors;
         expect(error.message).toBe(
-            'Variable "$data" got invalid value { description: "This is my 1st habit.", habitScore: 1 }; Field title of required type String! was not provided.'
+            'Variable "$data" got invalid value { description: "This is my 1st habit.", habitScore: null }; Field title of required type String! was not provided.'
         );
 
         // Even though the title is provided, it is still too short (because of the validation
@@ -103,7 +103,7 @@ describe("CRUD Test", () => {
             body: {
                 query: CREATE_HABIT,
                 variables: {
-                    data: { title: "Aa", description: "This is my 1st habit.", habitScore: 1 }
+                    data: { title: "Aa", description: "This is my 1st habit.", habitScore: null }
                 }
             }
         });
--- api/habits-tracker/src/plugins/models/habit.model.ts
+++ api/habits-tracker/src/plugins/models/habit.model.ts
@@ -15,11 +15,12 @@ export default ({ createBase }) =>
             // A simple "string" field, with a couple of validators attached.
             title: string({ validation: validation.create("required,minLength:3,maxLength:100") }),
 
+
+            // A simple "number" field, with the default value set to 1.
+            habitScore: number(),
+
             // This field is not required.
             description: string({ validation: validation.create("maxLength:500") }),
-
-            // A simple "boolean" field, with the default value set to "true".
-            habitScore: number()
         })),
         withHooks({
             // We might want to do something before saving the data to a database.
